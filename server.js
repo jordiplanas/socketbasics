cfd9d18c575cb7395b9d13a9857ca5cfd03b53e8
Jordi Vimod <jordi@vimod.net>
2016-08-03 11:33:51 +0200

    io

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-var PORT=3000|| process.env.PORT;
-var express=require('express');
-var app= express();
-var http=require('http').Server(app);
+var PORT = process.env.PORT || 3000;
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname + '/public'));
 
-http.listen(PORT,function(){
-	console.log("listenning on"+PORT);
-})
+io.on('connection', function() {
+	console.log('user connected')
+});
+
+http.listen(PORT, function() {
+	console.log("listenning on  " + PORT);
+});
